e12ba258f9305c9be567ed8ba2c0083580126be4
YevhenDenysov <yevhen0608@gmail.com>
2023-02-24 09:25:32 +0200

    remove styled-component in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { ConnectKitButton } from "connectkit";
-import styled from "styled-components";
 import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
